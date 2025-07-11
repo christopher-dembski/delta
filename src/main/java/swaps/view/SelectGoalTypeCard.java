80f475bfdfa077ddb5bd5c7b645889bfece17680
christopher-dembski <christopher_dembski@outlook.com>
2025-07-11 00:20:31 -0400

    Adjust styling.

--- src/main/java/swaps/view/SelectGoalTypeCard.java
+++ src/main/java/swaps/view/SelectGoalTypeCard.java
@@ -1,19 +1,26 @@
 package swaps.view;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.function.Consumer;
 
 public class SelectGoalTypeCard extends JPanel implements ISelectGoalTypeCard {
     private JComboBox goalTypeDropDown;
 
     protected SelectGoalTypeCard(int goalNumber) {
+        // vertically stack components
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.add(new JLabel("Select Goal Type %d".formatted(goalNumber)));
         DropdownOptionGoalType[] choices = {
                 DropdownOptionGoalType.PRECISE,
                 DropdownOptionGoalType.IMPRECISE
         };
         goalTypeDropDown = new JComboBox<>(choices);
-        this.add(goalTypeDropDown);
+        // wrap in JPanel with FlowLayout to avoid combo box filling entire window
+        JPanel comboWrapper = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        comboWrapper.add(new JLabel("Select Goal Type:"));
+        comboWrapper.add(goalTypeDropDown);
+        this.add(comboWrapper);
     }
 
     @Override
