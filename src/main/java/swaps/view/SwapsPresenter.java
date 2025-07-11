be071864f416cb7b16947a31b4b3215ee30273b6
christopher-dembski <christopher_dembski@outlook.com>
2025-07-11 00:00:38 -0400

    Enable creating two goals.

--- src/main/java/swaps/view/SwapsPresenter.java
+++ src/main/java/swaps/view/SwapsPresenter.java
@@ -5,13 +5,16 @@ import javax.swing.*;
 public class SwapsPresenter {
     // static variables
     private static final int FIRST_STEP = 1;
-    private static final int LAST_STEP = 2;
+    private static final int LAST_STEP = 5;
 
     // instance variables
     private ISwapsView view;
     private int currentStep;
-    private DropdownOptionGoalType selectedGoalType;
-    private DropdownOptionGoalIntensity selectedGoalIntensity;
+    private DropdownOptionGoalType selectedGoal1Type;
+    private DropdownOptionGoalIntensity selectedGoal1Intensity;
+    private DropDownCreateSecondGoal createSecondGoalYesNo;
+    private DropdownOptionGoalType selectedGoal2Type;
+    private DropdownOptionGoalIntensity selectedGoal2Intensity;
 
     public SwapsPresenter(ISwapsView view) {
         this.view = view;
@@ -23,20 +26,35 @@ public class SwapsPresenter {
     private void initDropDowns() {
         // set state
         // call setters to ensure presenter and view agree on state
-        selectedGoalType = DropdownOptionGoalType.IMPRECISE;
-        view.getSelectGoalTypeCard().setSelectedGoalType(selectedGoalType);
-        selectedGoalIntensity = DropdownOptionGoalIntensity.HIGH;
-        view.getCreateImpreciseGoalCard().setSelectedGoalIntensity(selectedGoalIntensity);
+        selectedGoal1Type = DropdownOptionGoalType.IMPRECISE;
+        view.getSelectGoal1TypeCard().setSelectedGoalType(selectedGoal1Type);
+        selectedGoal1Intensity = DropdownOptionGoalIntensity.HIGH;
+        view.getCreateImpreciseGoal1Card().setSelectedGoalIntensity(selectedGoal1Intensity);
+        createSecondGoalYesNo = DropDownCreateSecondGoal.NO;
+        view.getChooseOneOrTwoGoalsCard().setOneOrTwoGoalsDropdown(createSecondGoalYesNo);
+        selectedGoal2Type = DropdownOptionGoalType.IMPRECISE;
+        view.getSelectGoal2TypeCard().setSelectedGoalType(selectedGoal2Type);
+        selectedGoal2Intensity = DropdownOptionGoalIntensity.HIGH;
+        view.getCreateImpreciseGoal2Card().setSelectedGoalIntensity(selectedGoal2Intensity);
     }
 
     private void initActionListeners() {
         view.addNextButtonListener(e -> changeStep(currentStep + 1));
         view.addPreviousButtonListener(e -> changeStep(currentStep - 1));
-        view.getSelectGoalTypeCard().addGoalTypeDropDownListener(goalTypeFromDropDown -> {
-            selectedGoalType = goalTypeFromDropDown;
+        view.getSelectGoal1TypeCard().addGoalTypeDropDownListener(goalTypeFromDropDown -> {
+            selectedGoal1Type = goalTypeFromDropDown;
         });
-        view.getCreateImpreciseGoalCard().addGoalIntensityDropdownListener(goalIntensityFromDropdown -> {
-            selectedGoalIntensity = goalIntensityFromDropdown;
+        view.getCreateImpreciseGoal1Card().addGoalIntensityDropdownListener(goalIntensityFromDropdown -> {
+            selectedGoal1Intensity = goalIntensityFromDropdown;
+        });
+        view.getChooseOneOrTwoGoalsCard().addOneOrTwoGoalsDropdownListener(createSecondGoalYesNoFromDropdown -> {
+            createSecondGoalYesNo = createSecondGoalYesNoFromDropdown;
+        });
+        view.getSelectGoal2TypeCard().addGoalTypeDropDownListener(goalTypeFromDropDown -> {
+            selectedGoal2Type = goalTypeFromDropDown;
+        });
+        view.getCreateImpreciseGoal2Card().addGoalIntensityDropdownListener(goalIntensityFromDropdown -> {
+            selectedGoal2Intensity = goalIntensityFromDropdown;
         });
     }
 
@@ -50,11 +68,30 @@ public class SwapsPresenter {
                 break;
             }
             case 2: {
-                SwapWorkflowStep newPanelId = selectedGoalType.equals(DropdownOptionGoalType.PRECISE)
-                        ? SwapWorkflowStep.PRECISE_GOAL_DETAILS
-                        : SwapWorkflowStep.IMPRECISE_GOAL_DETAILS;
+                SwapWorkflowStep newPanelId = selectedGoal1Type.equals(DropdownOptionGoalType.PRECISE)
+                        ? SwapWorkflowStep.CREATE_GOAL_1_PRECISE
+                        : SwapWorkflowStep.CREATE_GOAL_1_IMPRECISE;
                 view.setCard(newPanelId);
                 break;
+            }
+            case 3: {
+                view.setCard(SwapWorkflowStep.CHOOSE_ONE_OR_TWO_GOALS);
+                break;
+            }
+            case 4: {
+                if (createSecondGoalYesNo.equals(DropDownCreateSecondGoal.NO)) {
+                    // skip creating second goal
+                    changeStep(7);
+                    break;
+                }
+                view.setCard(SwapWorkflowStep.SELECT_GOAL_2_TYPE);
+                break;
+            }
+            case 5: {
+                SwapWorkflowStep newCardId = selectedGoal2Type.equals(DropdownOptionGoalType.PRECISE)
+                        ? SwapWorkflowStep.CREATE_GOAL_2_PRECISE
+                        : SwapWorkflowStep.CREATE_GOAL_2_IMPRECISE;
+                view.setCard(newCardId);
             }
         }
     }
