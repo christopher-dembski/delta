f6db2f8e39083ec81615208b12f666bfcc145df2
christopher-dembski <christopher_dembski@outlook.com>
2025-07-11 00:20:31 -0400

    Adjust styling.

--- src/main/java/swaps/view/CreateImpreciseGoalCard.java
+++ src/main/java/swaps/view/CreateImpreciseGoalCard.java
@@ -1,12 +1,15 @@
 package swaps.view;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.function.Consumer;
 
 public class CreateImpreciseGoalCard extends JPanel implements ICreateImpreciseGoalCard {
     private JComboBox<DropdownOptionGoalIntensity> goalIntensityDropdown;
 
     protected CreateImpreciseGoalCard(int goalNumber) {
+        // vertically stack components
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.add(new JLabel("Create Goal %d (Imprecise)".formatted(goalNumber)));
         DropdownOptionGoalIntensity[] dropdownChoiceIntensityOptions = {
                 DropdownOptionGoalIntensity.HIGH,
@@ -14,7 +17,11 @@ public class CreateImpreciseGoalCard extends JPanel implements ICreateImpreciseG
                 DropdownOptionGoalIntensity.LOW
         };
         goalIntensityDropdown = new JComboBox<>(dropdownChoiceIntensityOptions);
-        this.add(goalIntensityDropdown);
+        // wrap in JPanel with FlowLayout to avoid combo box filling entire window
+        JPanel comboWrapper = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        comboWrapper.add(new JLabel("Select Goal Intensity:"));
+        comboWrapper.add(goalIntensityDropdown);
+        this.add(comboWrapper);
     }
 
     @Override
