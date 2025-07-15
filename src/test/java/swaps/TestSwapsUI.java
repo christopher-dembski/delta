aa9f3fd7818e804c883d3a8572e13202036f349c
christopher-dembski <christopher_dembski@outlook.com>
2025-07-14 20:14:42 -0400

    Add basic tests for swaps UI.

--- src/test/java/swaps/TestSwapsUI.java
+++ src/test/java/swaps/TestSwapsUI.java
@@ -1,16 +1,32 @@
 package swaps;
 
-
 import org.junit.jupiter.api.BeforeEach;
-import swaps.ui.*;
+import org.junit.jupiter.api.Test;
+import swaps.ui.SwapsPresenter;
+import swaps.ui.SwapsView;
+import swaps.ui.goals.CreateGoalsView;
 
-class TestSwapsUI {
-    private SwapsPresenter presenter;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+public class TestSwapsUI {
     private SwapsView view;
 
     @BeforeEach
     public void beforeEach() {
-         // view = spy(new SwapsView());
-         presenter = new SwapsPresenter(view);
+        view = spy(new SwapsView(new CreateGoalsView()));
+        new SwapsPresenter(view);  // initialize state and add action listeners to view
+    }
+
+    @Test
+    public void testDisablesPreviousButtonAndEnablesNextButtonOnFirstCard() {
+        verify(view).setPreviousButtonEnabled(false);
+        verify(view).setNextButtonEnabled(true);
+    }
+
+    @Test
+    public void testEnablesPreviousButtonAndEnablesNextButtonOnMiddleCard() {
+        verify(view).setPreviousButtonEnabled(false);
+        verify(view).setNextButtonEnabled(true);
     }
 }
