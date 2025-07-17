27fd9378cbcdb44399cd5697fbe55e61fd236d7d
christopher-dembski <christopher_dembski@outlook.com>
2025-07-17 16:23:45 -0400

    Use singleton presenter for app main presenter.

--- src/main/java/app/AppMainPresenter.java
+++ src/main/java/app/AppMainPresenter.java
@@ -4,6 +4,8 @@ import shared.NavigationPresenter;
 import shared.NavigationView;
 
 public class AppMainPresenter {
+    private static AppMainPresenter instance;
+
     private AppMainView appMainView;
 
     public AppMainPresenter() {
@@ -13,7 +15,14 @@ public class AppMainPresenter {
         leftNavPresenter.addNavigationListener(menuItem -> appMainView.renderCard(menuItem));
     }
 
+    public static AppMainPresenter instance() {
+        if (instance == null) {
+            instance = new AppMainPresenter();
+        }
+        return instance;
+    }
+
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(AppMainPresenter::new);
+        javax.swing.SwingUtilities.invokeLater(AppMainPresenter::instance);
     }
 }
