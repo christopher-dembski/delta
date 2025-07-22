03e1e616c6370d37362998f4a3bad86a973e648f
ngnamquoc <nguyennamquoc.mar@gmail.com>
2025-07-22 16:37:05 -0400

    seed mock user

--- src/main/java/data/seed-database.sql
+++ src/main/java/data/seed-database.sql
@@ -9,3 +9,9 @@ INSERT INTO students
 VALUES (3, 'Jae');
 INSERT INTO students
 VALUES (4, 'Abdullah');
+
+-- seed profiles table with 1 test user
+INSERT INTO profiles (full_name, age, sex, dob, height, weight, unit_system)
+VALUES 
+    ('John Doe', 25, 'MALE', '1999-07-22', 175.0, 70.0, 'METRIC'),
+
